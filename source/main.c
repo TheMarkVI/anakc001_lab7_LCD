b53098de7859a95b0bde45fec91b7d2b019de55c
TheMarkVI <arnak9167@gmail.com>
2021-10-24 19:18:58 -0700

    Initial Commit

---
+++ source/main.c
@@ -0,0 +1,27 @@
+/*	Author: Artip Nakchinda
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *  Video Demo: 
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include "io.h"
+#include "timer.h"
+
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
